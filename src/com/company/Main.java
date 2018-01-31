fb674f8e337bc4eda3ccbf5bdc8ee717de12d16c
Andrius <andriuscere@gmail.com>
2018-01-31 12:12:13 +0200

    Uzduotis0303

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        float suma=0;
+        float sk=0;
+        do{
+            System.out.println("Iveskite skaicius, kol bus 0");
+            Scanner sc = new Scanner(System.in);
+            sk=sc.nextFloat();
+            suma += sk;
+                } while (sk!=0);
+            System.out.println("Suma: "+suma);
+        }
+        }
+
